99f982012ca305541a705bd87a733a59cda139f5
Shahid Shah <shahshahid2123@gmail.com>
2026-02-24 01:00:00 +0530

    chore: commit current workspace changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
 module.exports = {
-  content: ["./index.html"],
-  theme: {
-    extend: {
-      colors: {
-        primary: '#4f46e5'
-      }
-    }
-  },
-  plugins: []
+    content: ["./index.html"],
+    theme: {
+        extend: {
+            colors: {
+                primary: '#4f46e5'
+            }
+        }
+    },
+    plugins: []
 }
